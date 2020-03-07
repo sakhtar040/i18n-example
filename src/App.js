931fcd6a7b5e2ca2b58131173a936f84b9f2920c
sakhtar <sakhtar@adobe.com>
2020-03-07 12:30:36 +0530

    i18n implementation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { I18nProvider, LOCALES } from "./i18n";
+import { FormattedMessage } from "react-intl";
+
+export class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			locale: LOCALES.ENGLISH
+		};
+
+		this.onChangeLocale.bind(this);
+	}
+
+	onChangeLocale = locale => {
+		switch (locale) {
+			case "ENGLISH":
+				this.setState({ locale: LOCALES.ENGLISH });
+				break;
+
+			case "FRENCH":
+				this.setState({ locale: LOCALES.FRENCH });
+				break;
+
+			default:
+				this.setState({ locale: LOCALES.ENGLISH });
+		}
+	};
+
+	render() {
+		return (
+			<I18nProvider locale={this.state.locale}>
+				<div className="App">
+					<header className="App-header">
+						<FormattedMessage id="hello" />
+						<img src={logo} className="App-logo" alt="logo" />
+						<p>
+							<FormattedMessage
+								id="edit"
+								values={{ path: <code>src/App.js</code> }}
+							/>
+						</p>
+						<a
+							className="App-link"
+							href="https://reactjs.org"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Learn React
+						</a>
+						<br />
+						<button onClick={() => this.onChangeLocale("ENGLISH")}>
+							ENGLISH
+						</button>
+						<br />
+						<button onClick={() => this.onChangeLocale("FRENCH")}>
+							FRENCH
+						</button>
+					</header>
+				</div>
+			</I18nProvider>
+		);
+	}
 }
 
 export default App;
---
+++ src/i18n/messages/fr-CA.js
@@ -0,0 +1,8 @@
+import { LOCALES } from "../locales";
+
+export default {
+	[LOCALES.FRENCH]: {
+        hello: "Bonjour",
+        edit: 'Modify {path} && savee to qwerty.'
+	}
+};
